f4239d6905f37abfc4b033334255744a77b66fa7
clarkngo <clarkngo@gmail.com>
2018-09-15 06:32:08 +0000

    pagination 10 to 3

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all.paginate(page: params[:page], per_page: 10)
+    @places = Place.all.paginate(page: params[:page], per_page: 3)
   end
 end
