59853a2aec9da0289024d47f2967b94fcf0c3572
clarkngo <clarkngo@gmail.com>
2018-09-28 23:10:18 +0000

    setup production

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,6 @@
 class NotificationMailer < ApplicationMailer
-
+  default from: "no-reply@nomsterapp.com"
+  
   def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
