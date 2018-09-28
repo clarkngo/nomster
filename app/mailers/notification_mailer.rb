7629313cf3ebaaed35ab8fe4df4d1f67307a0797
clarkngo <clarkngo@gmail.com>
2018-09-28 21:39:02 +0000

    notif mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,10 @@
 class NotificationMailer < ApplicationMailer
+
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    # + only works for gmail accounts
+    mail(to: "@place_owner.email",
+      subject: "A comment has been added to #{@place.name}")
+  end
 end
