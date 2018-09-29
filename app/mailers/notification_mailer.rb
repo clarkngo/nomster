43c9bc83c9ab71045321ee5a222911aa15fec5e9
clarkngo <clarkngo@gmail.com>
2018-09-29 04:42:00 +0000

    setup production

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,11 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
-  
+
   def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
     # + only works for gmail accounts
-    mail(to: "@place_owner.email",
+    mail(to: @place_owner.email,
       subject: "A comment has been added to #{@place.name}")
   end
 end
