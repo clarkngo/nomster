3f5cfa29db2526f6958a5e0fd3f13a207b3a050e
clarkngo <clarkngo@gmail.com>
2018-09-29 16:28:52 +0000

    email fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,16 +86,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
  # Setup the mailer config
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+   ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
   }
 
 
