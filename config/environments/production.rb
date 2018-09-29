02aff4b0e9d14bad5f9efb5accfdceb4c5a80eab
clarkngo <clarkngo@gmail.com>
2018-09-29 15:33:55 +0000

    email setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
-  }
-
   config.action_mailer.default_url_options = { host: 'nomster-clark-ngo.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -94,4 +84,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+
+
 end
